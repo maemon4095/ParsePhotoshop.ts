63ddcb99c3e67db45beb956e7cf3773593808809
maemon4095 <maemondecocted@gmail.com>
2024-05-28 14:10:27 +0900

    decompressRLE performance improvement

--- src/compression/mod.ts
+++ src/compression/mod.ts
@@ -1,6 +1,8 @@
 /** @see {@link https://www.fileformat.info/format/tiff/corion-packbits.htm} */
-export function decompressRLE(bin: Uint8Array): Uint8Array {
-    const buf: number[] = [];
+export function decompressRLE(bin: Uint8Array): Uint8Array { // ここに非常に時間がかかっている模様。
+    const totalLen = getTotalLength(bin);
+    const buf = new Uint8Array(totalLen);
+    let written = 0;
     let idx = 0;
     while (idx < bin.length) {
         const header = bin[idx];
@@ -11,15 +13,38 @@ export function decompressRLE(bin: Uint8Array): Uint8Array {
         if (header > 127) {
             const count = 256 - header + 1;
             const byte = bin[idx];
-            for (let i = 0; i < count; ++i) {
-                buf.push(byte);
-            }
+            buf.fill(byte, written, written + count);
+            written += count;
             idx += 1;
         } else {
-            buf.push(...bin.slice(idx, idx + header + 1));
-            idx += header + 1;
+            const count = header + 1;
+            buf.set(bin.slice(idx, idx + header + 1), written);
+            written += count;
+            idx += count;
         }
     }
 
     return new Uint8Array(buf);
+}
+
+function getTotalLength(bin: Uint8Array): number {
+    let len = 0;
+    let idx = 0;
+    while (idx < bin.length) {
+        const header = bin[idx];
+        idx += 1;
+        if (header === 128) {
+            continue;
+        }
+        if (header > 127) {
+            const count = 256 - header + 1;
+            len += count;
+            idx += 1;
+        } else {
+            const count = header + 1;
+            len += count;
+            idx += count;
+        }
+    }
+    return len;
 }
